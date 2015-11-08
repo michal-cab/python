a13173d62bc0b14359747a1a8de8c6322048d0e1
michal-cab <michal.cab@gmail.com>
2015-11-08 09:57:50 +0100

    modified:   README.md 	modified:   dot.png 	modified:   sinus_subpixel.py

--- sinus_subpixel/README.md
+++ sinus_subpixel/README.md
@@ -1,7 +1,14 @@
 variation of w. mc gugan code
 
+keys: LEFT ARROw, RIGHT ARROW = speed
+      UP ARROW, DOWN ARROW = amount of particles
+      c = white with black mask / random gray color
+      space = pause
+
 ![alt tag](http://ticho.multiplace.org/pic/sinus_subpixel/sinus_variations_02.png)
 
-TODO: - add live parameters 
+TODO: - add more live parameters -> curve variations
+
       - MIDI / OSC
+
       - send dot position
--- sinus_subpixel/sinus_subpixel.py
+++ sinus_subpixel/sinus_subpixel.py
@@ -5,36 +5,62 @@ import pygame
 from pygame.locals import *
 from subpixelsurface import *
 from math import sin, cos
+import random
 
-DOTS = 100
-
+def colorize(image, switch):
+    image = image.copy()
+    if switch == 0:
+        image.fill((255,255,255,255),None,pygame.BLEND_RGBA_MULT)
+    if switch == 1:
+        color1 = random.randint(0,100)
+        image.fill((color1,color1,color1,10),None,pygame.BLEND_RGBA_ADD)
+    return image
 
 def run():
+    loop = True
+    dots = 100
+    switch = 0
+    pause = 1
+    t = 0.
+    time_factor = 1 
 
     pygame.init()
     screen = pygame.display.set_mode((640, 480))
-    #screen = pygame.display.set_mode((640, 480),FULLSCREEN)
     clock = pygame.time.Clock()
-    dot = pygame.image.load("dot.png")
-    dot_subpixel = SubPixelSurface(dot, x_level=8)
 
-    t = 0.
-    while True:
+    while loop:
         for event in pygame.event.get():
             if event.type == QUIT:
-                return
+                loop = False
+            elif event.type == KEYDOWN:
+                if event.key == K_ESCAPE:
+                    loop = False
+                if event.key == K_UP:
+                    dots += 10
+                if event.key == K_DOWN:
+                    dots -= 10
+                if event.key == K_LEFT:
+                    time_factor -= .1
+                if event.key == K_RIGHT:
+                    time_factor += .1
+                if event.key == K_c:
+                    switch = (switch + 1) % 2
+                if event.key == K_SPACE:
+                    pause = (pause + 1) % 2
 
         time_passed = clock.tick()
-        t += time_passed / 3000.
-
-        for n in range(DOTS):
-            a = float(n)/DOTS * sin((t)*.1234)*100
+        dot = pygame.image.load("dot.png")
+        colorized_dot = colorize(dot, switch)
+        dot_subpixel = SubPixelSurface(colorized_dot, x_level=4)
+        t += time_factor * (time_passed / 3000.) * pause
+        
+        for n in range(dots):
+            a = float(n)/dots * sin((t)*.1234)*100
             x = sin((t+a)*sin(t/4)) * 200.*sin(t/5) + 320
-            y = cos(((t*1.234)+a)*sin(t/8)) * 200.*sin(t/4) + 220
+            y = sin(((t*1.234)+a)*sin(t/8)) * 200.*sin(t/4) + 220
             screen.blit(dot_subpixel.at(x, y), (x, y))
-
         pygame.display.update()
-
 
 if __name__ == "__main__":
     run()
+    pygame.quit()
